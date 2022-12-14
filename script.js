4df3e025b972df81f81e3b57d40ca08c999d8d16
Dantallope <dantebraccialini@gmail.com>
2022-12-13 20:33:10 -0700

    Added in the five day foracast cards and linked them with the information from the API

--- script.js
+++ script.js
@@ -30,31 +30,60 @@ function getWeather(data) {
     console.log(url)
 
     fetch(url)
-            .then(resp => {
-                if (!resp.ok) throw new Error(resp.statusText);
-                return resp.json();
-            })
-            .then(data => {
-               showWeather(data);
-            })
-            .catch(console.err);
-    }
-    function showWeather(data){
-    
-       document.getElementById("cTitle").innerHTML = data.city.name+' '+  moment().format('M/D/YY');
-       document.getElementById('demo').src = 'http://openweathermap.org/img/wn/10d@2x.png'
-       document.getElementById("cTemp").innerHTML = data.list[0].main.temp + '°F';
-       document.getElementById("cWind").innerHTML = data.list[0].wind.speed + 'MPH';
-       document.getElementById("cHum").innerHTML = data.list[0].main.humidity + '%';
+        .then(resp => {
+            if (!resp.ok) throw new Error(resp.statusText);
+            return resp.json();
+        })
+        .then(data => {
+            showWeather(data);
+        })
+        .catch(console.err);
+}
+function showWeather(data) {
+    //Imputing current weather data into the "current" HTML card
+    document.getElementById("cTitle").innerHTML = data.city.name + ' ' + moment().format('M/D/YY');
+    document.getElementById('demo').src = 'http://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '@2x.png'
+    document.getElementById("cTemp").innerHTML = data.list[0].main.temp + '°F';
+    document.getElementById("cWind").innerHTML = data.list[0].wind.speed + 'MPH';
+    document.getElementById("cHum").innerHTML = data.list[0].main.humidity + '%';
+    // One day ahead
+    document.getElementById("date1").innerHTML = moment().add(1, 'days').format('M/D/YY');
+    document.getElementById('icon1').src = 'http://openweathermap.org/img/wn/' + data.list[1].weather[0].icon + '@2x.png'
+    document.getElementById("temp1").innerHTML = data.list[1].main.temp + '°F';
+    document.getElementById("wind1").innerHTML = data.list[1].wind.speed + 'MPH';
+    document.getElementById("hum1").innerHTML = data.list[1].main.humidity + '%';
+    // Two days ahead
+    document.getElementById("date2").innerHTML = moment().add(2, 'days').format('M/D/YY');
+    document.getElementById('icon2').src = 'http://openweathermap.org/img/wn/' + data.list[6].weather[0].icon + '@2x.png'
+    document.getElementById("temp2").innerHTML = data.list[6].main.temp + '°F';
+    document.getElementById("wind2").innerHTML = data.list[6].wind.speed + 'MPH';
+    document.getElementById("hum2").innerHTML = data.list[6].main.humidity + '%';
+    // Three days ahead
+    document.getElementById("date3").innerHTML = moment().add(3, 'days').format('M/D/YY');
+    document.getElementById('icon3').src = 'http://openweathermap.org/img/wn/' + data.list[14].weather[0].icon + '@2x.png'
+    document.getElementById("temp3").innerHTML = data.list[14].main.temp + '°F';
+    document.getElementById("wind3").innerHTML = data.list[14].wind.speed + 'MPH';
+    document.getElementById("hum3").innerHTML = data.list[14].main.humidity + '%';
+    // Four days ahead
+    document.getElementById("date4").innerHTML = moment().add(4, 'days').format('M/D/YY');
+    document.getElementById('icon4').src = 'http://openweathermap.org/img/wn/' + data.list[22].weather[0].icon + '@2x.png'
+    document.getElementById("temp4").innerHTML = data.list[22].main.temp + '°F';
+    document.getElementById("wind4").innerHTML = data.list[22].wind.speed + 'MPH';
+    document.getElementById("hum4").innerHTML = data.list[22].main.humidity + '%';
+    // Five days ahead
+    document.getElementById("date5").innerHTML = moment().add(5, 'days').format('M/D/YY');
+    document.getElementById('icon5').src = 'http://openweathermap.org/img/wn/' + data.list[30].weather[0].icon + '@2x.png'
+    document.getElementById("temp5").innerHTML = data.list[30].main.temp + '°F';
+    document.getElementById("wind5").innerHTML = data.list[30].wind.speed + 'MPH';
+    document.getElementById("hum5").innerHTML = data.list[30].main.humidity + '%';
 
-      
 
-        console.log(data.list[0])
-        console.log(data.list[1].weather)
-        console.log(data.list[6].weather)
-        console.log(data.list[14].weather)
-        console.log(data.list[22].weather)
-        console.log(data.list[30].weather)
 
-    }
-    console.log(moment().add(1,'days').format('M/D/YY'))
+    console.log(data.list[0])
+    console.log(data.list[1])
+    console.log(data.list[6].weather)
+    console.log(data.list[14].weather)
+    console.log(data.list[22].weather)
+    console.log(data.list[30].weather)
+
+}
